93ec0a844a7fdfc55fd364bc6fbccad5b8a4ae15
Carrie Forde <carrie@carrieforde.com>
2018-04-06 23:59:49 -0700

    Adds a theme toggle button

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import Component from './component';
 import './sass/main.scss';
 
 const component = new Component(preload, '#app'); // eslint-disable-line no-unused-vars
+
+const themeToggle = document.getElementById('themeToggle');
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('ai-humanity-theme');
+});

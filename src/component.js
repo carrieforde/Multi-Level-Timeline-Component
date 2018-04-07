9e6fb3fe27f09a3d98519adfc2677d938c4134a3
Carrie Forde <carrie@carrieforde.com>
2018-04-06 23:59:35 -0700

    Adds click handlers and basic toggle functionality

--- src/component.js
+++ src/component.js
@@ -2,8 +2,32 @@ class Component {
   constructor(data, selector) {
     this.data = data;
     this.selector = selector;
+    this.keyCodes = Object.freeze({
+      RETURN: 13,
+      ESC: 27,
+      SPACE: 32,
+      LEFT: 37,
+      UP: 38,
+      RIGHT: 39,
+      DOWN: 40
+    });
 
     this.render();
+  }
+
+  activatePanel(toggle) {
+    const parent = toggle.parentElement.parentElement;
+
+    // Update the attributes.
+    toggle.setAttribute('aria-expanded', 'true');
+    parent.classList.add('accordion__item--expanded');
+  }
+
+  deactivatePanel(toggle) {
+    const parent = toggle.parentElement.parentElement;
+
+    toggle.setAttribute('aria-expanded', 'false');
+    parent.classList.remove('accordion__item--expanded');
   }
 
   render() {
@@ -19,9 +43,10 @@ class Component {
           <h3 class="component__subtitle">
             ${subtitle}
           </h3>
-          <p class="intro">${intro}</p>
+          <p class="component__intro">${intro}</p>
         </header>
-        <div class="component__content">`;
+        <div class="component__content">
+          <ul class="accordion accordion--level-1 list-reset">`;
 
     function buildLevels(data, i = 0) {
       let levels = ``;
@@ -30,20 +55,19 @@ class Component {
 
       data.forEach(item => {
         let level = `
-        <ul class="accordion accordion--level-${i} list-reset">
-          <li class="accordion__item accordion__item--level-${i}">
-            <h3 id="${item.title}" class="accordion__title accordion__title--level-${i}">
-              <button class="toggle toggle--plus-minus toggle--level-${i}">
-                <span class="screen-reader-text">Toggle</span>
-              </button>
-              ${item.title}
-            </h3>
-            <div id="${item.title.toLowerCase()}Content" class="accordion__content accordion__content--level-${i}">
-              ${item.content instanceof Array ? buildLevels(item.content, i) : item.content ? item.content : ''}
-            </div>
-          </li>
-        </ul>
-      `;
+          ${i > 1 ? `<ul class="accordion accordion--level-${i} list-reset">` : ''}
+            <li class="accordion__item accordion__item--level-${i}">
+              <h3 id="${item.title}" class="accordion__title accordion__title--level-${i}">
+                <button class="toggle toggle--plus-minus toggle--level-${i}" aria-expanded="false" aria-controls="${item.title.toLowerCase()}Content">
+                  <span class="screen-reader-text">Toggle</span>
+                </button>
+                ${item.title}
+              </h3>
+              <div id="${item.title.toLowerCase()}Content" class="accordion__content accordion__content--level-${i}">
+                ${item.content instanceof Array ? buildLevels(item.content, i) : item.content ? item.content : ''}
+              </div>
+            </li>
+          ${i > 1 ? `</ul>` : ''}`;
 
         levels += level;
       });
@@ -57,6 +81,56 @@ class Component {
     </section>`;
 
     el.innerHTML = timeline;
+
+    el.addEventListener('click', this.handleClicks.bind(this));
+    el.addEventListener('keyup', this.handleKeyup.bind(this));
+  }
+
+  handleClicks(event) {
+    const target = event.target.closest('.toggle');
+
+    if (target && target.getAttribute('aria-expanded') === 'false') {
+      this.activatePanel(target);
+      return true;
+    }
+
+    if (target && target.getAttribute('aria-expanded') === 'true') {
+      this.deactivatePanel(target);
+      return true;
+    }
+
+    return false;
+  }
+
+  handleKeyup(event) {
+    const key = event.keyCode,
+      target = event.target,
+      parent = target.parentElement.parentElement,
+      accordion = parent.parentElement;
+
+    let newTarget;
+
+    switch (key) {
+      // These cases are handled by click event.
+      case this.keyCodes.RETURN:
+      case this.keyCodes.SPACE:
+        break;
+
+      case this.keyCodes.DOWN:
+        console.log(parent.nextElementSibling); // eslint-disable-line no-console
+        if (parent.nextElementSibling === null) {
+          newTarget = accordion.querySelector('.toggle');
+        } else {
+          newTarget = parent.nextElementSibling;
+          newTarget = newTarget.querySelector('.toggle');
+        }
+
+        newTarget.focus();
+        break;
+
+      default:
+        break;
+    }
   }
 }
 

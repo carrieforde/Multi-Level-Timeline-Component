6800a43d18e67f8fea59dad01b6b8c7d5d04a81a
Carrie Forde <carrie@carrieforde.com>
2018-04-07 00:01:54 -0700

    Build files :construction:

--- dist/bundle.js
+++ dist/bundle.js
@@ -84,6 +84,12 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var component = new _component2.default(_data2.default, '#app'); // eslint-disable-line no-unused-vars
 
+var themeToggle = document.getElementById('themeToggle');
+
+themeToggle.addEventListener('click', function () {
+  document.body.classList.toggle('ai-humanity-theme');
+});
+
 /***/ }),
 /* 1 */
 /***/ (function(module, exports) {
@@ -111,11 +117,37 @@ var Component = function () {
 
     this.data = data;
     this.selector = selector;
+    this.keyCodes = Object.freeze({
+      RETURN: 13,
+      ESC: 27,
+      SPACE: 32,
+      LEFT: 37,
+      UP: 38,
+      RIGHT: 39,
+      DOWN: 40
+    });
 
     this.render();
   }
 
   _createClass(Component, [{
+    key: 'activatePanel',
+    value: function activatePanel(toggle) {
+      var parent = toggle.parentElement.parentElement;
+
+      // Update the attributes.
+      toggle.setAttribute('aria-expanded', 'true');
+      parent.classList.add('accordion__item--expanded');
+    }
+  }, {
+    key: 'deactivatePanel',
+    value: function deactivatePanel(toggle) {
+      var parent = toggle.parentElement.parentElement;
+
+      toggle.setAttribute('aria-expanded', 'false');
+      parent.classList.remove('accordion__item--expanded');
+    }
+  }, {
     key: 'render',
     value: function render() {
       var el = document.querySelector(this.selector),
@@ -126,7 +158,7 @@ var Component = function () {
           data = _data.data;
 
 
-      var timeline = '\n      <section class="component component--roadmap">\n        <header class="component__header">\n          <h2 class="component__title">\n            ' + title + '\n          </h2>\n          <h3 class="component__subtitle">\n            ' + subtitle + '\n          </h3>\n          <p class="intro">' + intro + '</p>\n        </header>\n        <div class="component__content">';
+      var timeline = '\n      <section class="component component--roadmap">\n        <header class="component__header">\n          <h2 class="component__title">\n            ' + title + '\n          </h2>\n          <h3 class="component__subtitle">\n            ' + subtitle + '\n          </h3>\n          <p class="component__intro">' + intro + '</p>\n        </header>\n        <div class="component__content">\n          <ul class="accordion accordion--level-1 list-reset">';
 
       function buildLevels(data) {
         var i = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
@@ -136,7 +168,7 @@ var Component = function () {
         i++;
 
         data.forEach(function (item) {
-          var level = '\n        <ul class="accordion accordion--level-' + i + ' list-reset">\n          <li class="accordion__item accordion__item--level-' + i + '">\n            <h3 id="' + item.title + '" class="accordion__title accordion__title--level-' + i + '">\n              <button class="toggle toggle--plus-minus toggle--level-' + i + '">\n                <span class="screen-reader-text">Toggle</span>\n              </button>\n              ' + item.title + '\n            </h3>\n            <div id="' + item.title.toLowerCase() + 'Content" class="accordion__content accordion__content--level-' + i + '">\n              ' + (item.content instanceof Array ? buildLevels(item.content, i) : item.content ? item.content : '') + '\n            </div>\n          </li>\n        </ul>\n      ';
+          var level = '\n          ' + (i > 1 ? '<ul class="accordion accordion--level-' + i + ' list-reset">' : '') + '\n            <li class="accordion__item accordion__item--level-' + i + '">\n              <h3 id="' + item.title + '" class="accordion__title accordion__title--level-' + i + '">\n                <button class="toggle toggle--plus-minus toggle--level-' + i + '" aria-expanded="false" aria-controls="' + item.title.toLowerCase() + 'Content">\n                  <span class="screen-reader-text">Toggle</span>\n                </button>\n                ' + item.title + '\n              </h3>\n              <div id="' + item.title.toLowerCase() + 'Content" class="accordion__content accordion__content--level-' + i + '">\n                ' + (item.content instanceof Array ? buildLevels(item.content, i) : item.content ? item.content : '') + '\n              </div>\n            </li>\n          ' + (i > 1 ? '</ul>' : '');
 
           levels += level;
         });
@@ -147,6 +179,58 @@ var Component = function () {
       timeline += '\n        ' + buildLevels(data) + '\n      </div>\n    </section>';
 
       el.innerHTML = timeline;
+
+      el.addEventListener('click', this.handleClicks.bind(this));
+      el.addEventListener('keyup', this.handleKeyup.bind(this));
+    }
+  }, {
+    key: 'handleClicks',
+    value: function handleClicks(event) {
+      var target = event.target.closest('.toggle');
+
+      if (target && target.getAttribute('aria-expanded') === 'false') {
+        this.activatePanel(target);
+        return true;
+      }
+
+      if (target && target.getAttribute('aria-expanded') === 'true') {
+        this.deactivatePanel(target);
+        return true;
+      }
+
+      return false;
+    }
+  }, {
+    key: 'handleKeyup',
+    value: function handleKeyup(event) {
+      var key = event.keyCode,
+          target = event.target,
+          parent = target.parentElement.parentElement,
+          accordion = parent.parentElement;
+
+      var newTarget = void 0;
+
+      switch (key) {
+        // These cases are handled by click event.
+        case this.keyCodes.RETURN:
+        case this.keyCodes.SPACE:
+          break;
+
+        case this.keyCodes.DOWN:
+          console.log(parent.nextElementSibling); // eslint-disable-line no-console
+          if (parent.nextElementSibling === null) {
+            newTarget = accordion.querySelector('.toggle');
+          } else {
+            newTarget = parent.nextElementSibling;
+            newTarget = newTarget.querySelector('.toggle');
+          }
+
+          newTarget.focus();
+          break;
+
+        default:
+          break;
+      }
     }
   }]);
 
